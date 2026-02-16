349fd192ed8dd38e4f7fb651f9c4c30556ceee43
Zack <zacharycummings0714@gmail.com>
2026-02-15 22:13:58 -0700

    Remove unused assembly files
    Remove Executor as it was never utilized due to plans changing, and remove the old interrupt handler. The new interrupt handler does not use assembly, just physical wiring.

--- src/Code/Executor.asm
+++
@@ -1,6 +0,0 @@
-// keep a list of programs
-	// fixed-size array allowing a maximum of 10 programs to be executed
-	// it contains all registers and the pc
-	// this results in each entry being 30B
-// execute these programs in order
-// every clock interrupt, pause the current program and move to the next
